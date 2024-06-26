d69dfe0c5933684f36d706ebe07a5589f04509ac
Aniket-Agalave <aniketsa4510@gmail.com>
2024-06-26 19:28:51 +0530

    anagram js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,17 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.replace(/[^\w]/g, "").toLowerCase();
+  str2 = str2.replace(/[^\w]/g, "").toLowerCase();
 
-  if (str1.length != str2.length) {
+  if (str1.length !== str2.length) {
     return false;
   }
-  str1.sort();
-  str2.sort();
-  for (let i = 0; i <= str1.length; i++){
-    if (str1[i] == str2[i]) {
-      return true;
-    }
-  }
+
+  const newStr1 = str1.split("").sort().join();
+  const newStr2 = str2.split("").sort().join();
+
+  return newStr1 === newStr2;
 }
 
 module.exports = isAnagram;
