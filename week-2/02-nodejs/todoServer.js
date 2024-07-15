cef88c1fb21fe9c471fe4f16594fc1d1aed729d9
Aniket-Agalave <aniketsa4510@gmail.com>
2024-07-15 12:04:44 +0530

    Solved week-3 middleware assignments

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -39,11 +39,13 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  module.exports = app;
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+module.exports = app;
